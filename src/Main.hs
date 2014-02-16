cf4bd2fc98ac822776fe7c900da225b0ae448fb8
Simon Altschuler <simon@altschuler.dk>
2014-02-16 15:45:57 +0100

    proper command line args

--- src/Main.hs
+++ src/Main.hs
@@ -1,7 +1,25 @@
-import System.Environment
+{-# LANGUAGE DeriveDataTypeable, RecordWildCards #-}
 import Lexer
 import Parser
+import System.Console.CmdArgs
+import Control.Monad
 
-main = getArgs >>= \(t:prog:_) -> if t == "l"
-                                  then (print . alexScanTokens) prog
-                                  else (print . kiteparser . alexScanTokens) prog
+data KiteArgs = KiteArgs {
+  input :: String,
+  eval :: Bool,
+  lex :: Bool,
+  par :: Bool
+  } deriving (Data, Typeable, Show)
+
+kiteArgs = cmdArgsMode $ KiteArgs {
+  input = "" &= argPos 0 &= typ "file",
+  eval = False &= help "Evaluate expression",
+  lex = False &= help "Emit lexer output",
+  par = False &= help "Emit parser output"}
+           &= summary "Kite compiler v0.0.1"
+
+main = do
+  KiteArgs {..} <- cmdArgsRun kiteArgs
+  inp <- if eval then return input else readFile input
+  when lex $ (print . alexScanTokens) inp
+  when par $ (print . kiteparser . alexScanTokens) inp
