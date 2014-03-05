cd97578c321e77e57db5e1542911271d26afdc4e
kiaer <ckiaer@hotmail.com>
2014-03-05 23:33:52 +0100

    Added tests for type checker

--- tests/TypeCheckTest.hs
+++ tests/TypeCheckTest.hs
@@ -19,7 +19,7 @@ typeCheckTests = testGroup "Type Check"
   [ testCase "Assignment" $
     Nothing @?= analyze "one = 2"
 
-  , testCase "Illeagal assignment" $
+  , testCase "Illegal assignment" $
     Just TypeE @?= analyze "{ one = 2; one = \"the\"; }"
 
   , testCase "Reference" $
@@ -36,4 +36,31 @@ typeCheckTests = testGroup "Type Check"
 
   , testCase "Function call with wrong number of args" $
     Just ArE @?= analyze "{ one = () -> Int { return 1 }; one(1); }"
+
+  , testCase "Function call to undefined function" $
+    Just RefE @?= analyze "{ foo(2) }"
+
+  , testCase "Recursive function" $
+    Nothing @?= analyze "{ fib = (Int n) -> Int { return if n == 0 then 0 else if n == 1 then 1 else fib (n - 1) + fib (n - 2)}; fib(5); }"
+
+  ,  testCase "Recursive function wrong return type" $
+    Just TypeE @?= analyze "{ fib = (Int n) -> Bool { return if n == 0 then 0 else if n == 1 then 1 else fib (n - 1) + fib (n - 2)}; fib(5); }"
+
+  , testCase "List assignment same type" $
+    Nothing @?= analyze "{ list = [1, 2, 3] }"
+
+  , testCase "List assignment illegal values" $
+    Just TypeE @?= analyze "{ list = [1, True, \"Three\"] }"
+
+  , testCase "Check Bool operators" $
+    Nothing @?= analyze "{ foo = () -> Bool { return 2 > 1 }; }"
+
+  , testCase "Index of list" $
+    Nothing @?= analyze "{ foo = () -> Bool{ list = [True, False] return list # 1}; }"
+
+  , testCase "Index of list illegal argument" $
+    Just TypeE @?= analyze "{ foo = [1, 2, 3] # \"one\"}"
+
+  , testCase "Illegal arithmic operator" $
+    Just TypeE @?= analyze "{ list = [1, 2, 3] / [4, 5, 6]; s = \"test\" / \"string\"; a = 1 + \"two\" }"
   ]
