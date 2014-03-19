fcab75d9afe12a02543fae1b2520857091397811
Simon Altschuler <simon@altschuler.dk>
2014-03-19 21:24:28 +0100

    update test syntax

--- tests/TypeCheckTest.hs
+++ tests/TypeCheckTest.hs
@@ -24,16 +24,16 @@ typeCheckTests = testGroup "Type Check"
     Nothing "one = 2"
 
   , testE "Illegal reassignment"
-    (Just TypeE) "{ one = 2; one = \"the\"; }"
+    (Just TypeE) "one = 2; one = \"the\";"
 
   , testE "Reference"
-    Nothing "{ one = 1; two = 1 + one; }"
+    Nothing "one = 1; two = 1 + one;"
 
   , testE "No return statements"
-    (Just TypeE) "{ f = (a: Int) -> Int { 2 } }"
+    (Just TypeE) "f = (a: Int) -> Int { 2 }"
 
   , testE "Varying return types"
-    (Just TypeE) "{ f = (a: Int) -> Int { return 2; return 2.0 } }"
+    (Just TypeE) "f = (a: Int) -> Int { return 2; return 2.0 }"
 
   , testE "Reference not defined"
     (Just RefE) "two = 1 + one"
@@ -42,59 +42,59 @@ typeCheckTests = testGroup "Type Check"
     (Just RefE) "two = 1 + two"
 
   , testE "Function call"
-    Nothing "{ one = (a: Int) -> Int { return a }; one(1); }"
+    Nothing "one = (a: Int) -> Int { return a }; one(1)"
 
   , testE "Function call with arg of wrong type"
-    (Just TypeE) "{ one = (a: Int) -> Int { return a }; one(\"1\"); }"
+    (Just TypeE) "one = (a: Int) -> Int { return a }; one(\"1\")"
 
   , testE "Function call with multiple parameters"
-    Nothing "{ foo = (a: Int, b: Int) -> Int { return 1 }; foo(1, 2) }"
+    Nothing "foo = (a: Int, b: Int) -> Int { return 1 }; foo(1, 2)"
 
   , testE "Function call with multiple parameters and wrong arity"
-    (Just ArE) "{ foo = (a: Int, b: Int) -> Int { return 1 }; foo(1) }"
+    (Just ArE) "foo = (a: Int, b: Int) -> Int { return 1 }; foo(1)"
 
   , testE "Function call with wrong number of args"
-    (Just ArE) "{ one = () -> Int { return 1 }; one(1); }"
+    (Just ArE) "one = () -> Int { return 1 }; one(1)"
 
   , testE "Function call to undefined function"
-    (Just RefE) "{ foo(2) }"
+    (Just RefE) "foo(2)"
 
   , testE "Recursive function"
-    Nothing "{ fib = (n: Int) -> Int { return if n == 0 then 0 else if n == 1 then 1 else fib (n - 1) + fib (n - 2)}; fib(5); }"
+    Nothing "fib = (n: Int) -> Int { return if n == 0 then 0 else if n == 1 then 1 else fib (n - 1) + fib (n - 2)}; fib(5)"
 
   , testE "Recursive function wrong return type"
-    (Just TypeE) "{ fib = (n: Int) -> Bool { return if n == 0 then 0 else if n == 1 then 1 else fib (n - 1) + fib (n - 2)}; fib(5); }"
+    (Just TypeE) "fib = (n: Int) -> Bool { return if n == 0 then 0 else if n == 1 then 1 else fib (n - 1) + fib (n - 2)}; fib(5)"
 
   , testE "List assignment same type"
-    Nothing "{ list = [1, 2, 3] }"
+    Nothing "list = [1, 2, 3]"
 
   , testE "List assignment illegal values"
-    (Just TypeE) "{ list = [1, True, \"Three\"] }"
+    (Just TypeE) "list = [1, True, \"Three\"]"
 
   , testE "Append to list"
-    Nothing "{ list = [1, 2] + [3] }"
+    Nothing "list = [1, 2] + [3]"
 
   , testE "Append to list with wrong type"
-    (Just TypeE) "{ list = [1, 2] + 3.0 }"
+    (Just TypeE) "list = [1, 2] + 3.0"
 
   , testE "Concatenate string"
-    Nothing "{ s = \"str\" + \"ing\" }"
+    Nothing "s = \"str\" + \"ing\""
 
   , testE "Concatenate string with numbers in sting"
-    Nothing "{ s = \"2\" + \"2.0\" }"
+    Nothing "s = \"2\" + \"2.0\""
 
   , testE "Concatenate string with numbers"
-    (Just TypeE) "{ s = \"2\" + 2.0 }"
+    (Just TypeE) "s = \"2\" + 2.0"
 
   , testE "Check Bool operators"
-    Nothing "{ foo = () -> Bool { return 2 > 1 } }"
+    Nothing "foo = () -> Bool { return 2 > 1 }"
 
   , testE "Index of list"
-    Nothing "{ foo = () -> Bool { list = [True, False] return list # 1} }"
+    Nothing "foo = () -> Bool { list = [True, False] return list # 1}"
 
   , testE "Index of list illegal argument"
-    (Just TypeE) "{ foo = [1, 2, 3] # \"one\"}"
+    (Just TypeE) "foo = [1, 2, 3] # \"one\""
 
   , testE "Illegal arithmic operator"
-    (Just TypeE) "{ list = [1, 2, 3] / [4, 5, 6]; s = \"test\" / \"string\"; a = 1 + \"two\" }"
+    (Just TypeE) "list = [1, 2, 3] / [4, 5, 6]; s = \"test\" / \"string\"; a = 1 + \"two\""
   ]
