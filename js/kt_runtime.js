66d2eb418dcdac85c3d7f3cb72884088109922f9
Simon Altschuler <simon@altschuler.dk>
2014-04-18 18:57:30 +0200

    add the js runtime

---
+++ js/kt_runtime.js
@@ -0,0 +1,44 @@
+
+var print = function (str) { console.log(str); };
+var length = function (xs) { return xs.length; };
+var slice = function (xs){
+    return function(from) {
+        return function(to) {
+            return xs.slice(from, to);
+        };
+    };
+};
+
+var KT_COLON = function (x) {
+    return function (xs) {
+        var len = xs.length;
+        /* TODO: this isn't a deep clone, also, this is ridicously inefficient */
+        var clone = [];
+        for (var i = 0; i < len; i++) {
+            clone.push(xs[i]);
+        }
+        clone.unshift(x);
+        return clone;
+    };
+};
+
+var KT_PLUS = function  (l) { return function (r) { return l + r; };};
+var KT_MINUS = function  (l) { return function (r) { return l - r; };};
+var KT_STAR = function  (l) { return function (r) { return l * r;};};
+var KT_SLASH = function  (l) { return function (r) { return l / r;};};
+
+var KT_PERCENT = function  (l) { return function (r) { return l % r;};};
+
+var KT_POUND = function  (arr) { return function (idx) { return arr[idx];}; };
+
+var KT_EQKT_EQ = function  (l) { return function (r) { return l === r;};};
+var KT_LTKT_EQ = function  (l) { return function (r) { return l <= r;};};
+
+
+var KT_IF = function (cond) {
+    return function (conseq) {
+        return function (alt) {
+            if (cond()) return conseq(); else return alt();
+        };
+    };
+};
