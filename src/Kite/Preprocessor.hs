2b6b18639b15d4f9ce7e8900d393f6fc7b423f54
Simon Altschuler <simon@altschuler.dk>
2014-04-20 10:42:05 +0200

    add the preprocessor, oops

---
+++ src/Kite/Preprocessor.hs
@@ -0,0 +1,30 @@
+module Kite.Preprocessor (preprocess) where
+
+import qualified Language.Preprocessor.Cpphs as Cpphs
+import System.Directory
+import System.FilePath.Posix
+import System.IO
+
+data Encoding = Encoding (Maybe (Handle -> IO ()))
+
+useEncoding h (Encoding x) = maybe (return ()) ($ h) x
+
+readFileEncoding enc file = do
+    h <- if file == "-" then return stdin else openFile file ReadMode
+    useEncoding h enc
+    hGetContents h
+
+options file = do
+  cwd <- getCurrentDirectory
+  let full = dropFileName $ joinPath [cwd, file]
+  case Cpphs.parseOptions [file, "-I" ++ full, "--noline"] of
+    Right opts -> return opts
+    Left err -> error err
+
+include file = do
+  source <- readFile file
+  enc <- readFileEncoding (Encoding Nothing) file
+  opts <- options file
+  Cpphs.runCpphs opts source enc
+
+preprocess = include
