501566f1b9140829b622b0d6cd48375e89b50845
Simon Altschuler <simon@altschuler.dk>
2014-06-04 17:45:42 +0200

    basic optimization

---
+++ src/Kite/Optimizer.hs
@@ -0,0 +1,106 @@
+module Kite.Optimizer where
+
+import Debug.Trace
+import Data.List
+import Data.Maybe
+
+import Control.Monad.State
+import Control.Monad.Error
+
+import Kite.Syntax
+
+data OptiError = OptiError String
+               | UnknownError
+               deriving (Show, Eq)
+
+instance Error OptiError where
+  noMsg = UnknownError
+  strMsg = OptiError
+
+throwOE :: String -> Opti a
+throwOE = throwError . OptiError
+
+type Name = String
+
+data Needs = Needs { fns :: [String] }
+
+data OptiState = OptiState { decls :: [Decl], scope :: [[Name]], checked :: [Name] }
+type Opti a = ErrorT OptiError (State OptiState) a
+
+builtInNames :: [Name]
+builtInNames = ["print"]
+
+builtIns :: [Decl]
+builtIns = map (`PDecl` PVoid) builtInNames
+
+findDecl :: Name -> Opti (Maybe Expr)
+findDecl name = do
+  st <- get
+  let decl = find (\d -> case d of
+                      PDecl dname _ -> dname == name) (decls st)
+  case decl of
+    Just (PDecl _ expr) -> return (Just expr)
+    Nothing -> return Nothing
+
+optimize :: [Decl] -> [Decl]
+optimize ds = do
+  let used = evalState (runErrorT optimize') OptiState { decls = ds ++ builtIns,
+                                                         scope = [],
+                                                         checked = [] }
+  case used of
+    Right names -> do
+      let keep = "main" : (builtInNames ++ names)
+      filter (\d -> case d of
+                              PDecl name _ -> name `elem` keep) ds
+
+optimize' :: Opti [Name]
+optimize' = do
+  main <- findDecl "main"
+  case main of
+    Just expr -> mread expr
+    Nothing -> throwOE "Can't optimize, no 'main' found"
+
+mread' :: [Expr] -> Opti [Name]
+mread' exprs = do
+  a <- mapM mread exprs
+  return $ concat a
+
+mread :: Expr -> Opti [Name]
+
+-- primitives
+mread PVoid         = return []
+mread (PInteger _)  = return []
+mread (PFloat _)    = return []
+mread (PChar _)     = return []
+mread (PBool _)     = return []
+
+-- base
+mread (PIdentifier name) = do
+  st <- get
+  if name `elem` checked st
+    then return [name]
+    else do
+    d <- findDecl name
+
+    sub <- case d of
+      Just expr -> do
+        put st { checked = name : checked st }
+        mread expr
+      Nothing -> return []
+
+    return $ name : sub
+
+  -- look in scope, then decls, mread recursively
+mread (PApply fn arg) = mread' [fn, arg]
+mread (PList exprs) = mread' exprs
+mread (PBlock exprs) = mread' exprs
+
+mread (PPair exprA exprB) = mread' [exprA, exprB]
+mread (PIf cond conseq alt) = mread' [cond, conseq, alt]
+mread (PBind name expr) = do
+  e <- mread expr
+  return (e \\ [name])
+
+mread (PLambda _ expr) = mread expr -- push scope
+mread (PReturn expr) = mread expr
+mread (PMatch expr cases) = mread' (expr : map snd cases)
